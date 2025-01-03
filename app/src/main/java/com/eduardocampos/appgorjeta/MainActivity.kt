184263eefd9eb9eb790f33f58d229b5263262517
Eduardo Campos <deoli.cmps@gmail.com>
2025-01-03 15:45:04 -0400

    Substituido as FindViewbyID por viewBinding e recuperado valores dos botões, além de calcular valor total com as tips

--- app/src/main/java/com/eduardocampos/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/eduardocampos/appgorjeta/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.eduardocampos.appgorjeta
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.eduardocampos.appgorjeta.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        var percentage: Int = 0
+
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            println("Edu1 Botao 1:$isChecked")
+            if (isChecked){
+                percentage = 10
+            }
         }
+
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            println("Edu1 Botao 2:$isChecked")
+            if (isChecked){
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            println("Edu1 Botao 3:$isChecked")
+            if (isChecked){
+                percentage = 20
+            }
+        }
+
+        binding.btnClean.setOnClickListener {
+
+        }
+
+        binding.btnDone.setOnClickListener {
+            val totalAmount: Float = binding.tieTotal.text.toString().toFloat()
+            val nPeople: Int = binding.tieNumPeople.text.toString().toInt()
+
+
+            val totalWithTips = totalAmount + ((totalAmount*percentage)/100)
+
+            println("Edu1 O total é: $totalWithTips")
+        }
+
     }
 }
