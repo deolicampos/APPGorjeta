c0750c8de52666d40031d58a4e152ea8b3a60d07
Eduardo Campos <deoli.cmps@gmail.com>
2025-01-16 10:51:27 -0400

    Adicionado um Spinner, para fins de aprendizado

--- app/src/main/java/com/eduardocampos/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/eduardocampos/appgorjeta/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.eduardocampos.appgorjeta
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.eduardocampos.appgorjeta.databinding.ActivityMainBinding
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        // lógica envolvida para o radio Button de selecionar porcentagem
         var percentage: Int = 0
 
         binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
@@ -41,23 +45,47 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnClean.setOnClickListener {
-
         }
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerNumberOfPeople.adapter = adapter
+
+        var numOfPeopleSelected = 0
+        binding.spinnerNumberOfPeople.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+            )   {
+                numOfPeopleSelected = position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
 
         binding.btnDone.setOnClickListener {
             val totalAmount = binding.tieTotal.text
-            val nPeople = binding.tieNumPeople.text
 
-            if (totalAmount?.isEmpty() == true || nPeople?.isEmpty() == true){
+            if (totalAmount?.isEmpty() == true){
                 Snackbar
                     .make(binding.tieTotal, "Preencha todos os Campos", Snackbar.LENGTH_LONG)
                     .show()
             } else {
                 val totalAmount: Float = binding.tieTotal.text.toString().toFloat()
-                val nPeople: Int = binding.tieNumPeople.text.toString().toInt()
+
 
                 val totalWithTips = totalAmount + ((totalAmount * percentage) / 100)
-                val totalPerClient = totalWithTips / nPeople
+                val totalPerClient = totalWithTips / numOfPeopleSelected
                 binding.tvResult.text = "Total with  tips per client: $totalPerClient"
             }
         }
@@ -65,7 +93,6 @@ class MainActivity : AppCompatActivity() {
         binding.btnClean.setOnClickListener {
             binding.tvResult.text = ""
             binding.tieTotal.setText("")
-            binding.tieNumPeople.setText("")
             binding.rbOptionOne.isChecked = false
             binding.rbOptionTwo.isChecked = false
             binding.rbOptionThree.isChecked = false
